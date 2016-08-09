fff2fcabb870c3aadb6a053be2d81eb65134bd82
LiuHaixin <liuhaixin2016@outlook.com>
2016-08-10 01:02:02 +0800

    Init

---
+++ CefSharpForm/CefSharpForm/BrowserForm.cs
@@ -0,0 +1,33 @@
+﻿using CefSharp;
+using CefSharp.WinForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CefSharpForm
+{
+    public partial class BrowserForm : Form
+    {
+        public BrowserForm()
+        {
+            InitializeComponent();
+            WindowState = FormWindowState.Maximized;
+            Cef.Initialize(new CefSettings());
+            string url = ConfigurationManager.AppSettings["Home"];
+            ChromiumWebBrowser browser = new ChromiumWebBrowser(url)
+            {
+                Dock = DockStyle.Fill
+            };
+            this.Controls.Add(browser);
+            browser.DownloadHandler = new DownloadHandler();
+            browser.MenuHandler = new MenuHandler();
+        }
+    }
+}
---
+++ CefSharpForm/CefSharpForm/MenuHandler.cs
@@ -0,0 +1,45 @@
+﻿using CefSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CefSharpForm
+{
+    public class MenuHandler : IContextMenuHandler
+    {
+        private const int ShowDevTools = 26501;
+        private const int CloseDevTools = 26502;
+
+        public void OnBeforeContextMenu(IWebBrowser browserControl, IBrowser browser, IFrame frame, IContextMenuParams parameters, IMenuModel model)
+        {
+            model.Clear();
+            model.AddItem((CefMenuCommand)ShowDevTools, "Show DevTools");
+            model.AddItem((CefMenuCommand)CloseDevTools, "Close DevTools");
+        }
+
+        public bool OnContextMenuCommand(IWebBrowser browserControl, IBrowser browser, IFrame frame, IContextMenuParams parameters, CefMenuCommand commandId, CefEventFlags eventFlags)
+        {
+            if ((int)commandId == ShowDevTools)
+            {
+                browser.ShowDevTools();
+            }
+            if ((int)commandId == CloseDevTools)
+            {
+                browser.CloseDevTools();
+            }
+            return false;
+        }
+
+        public void OnContextMenuDismissed(IWebBrowser browserControl, IBrowser browser, IFrame frame)
+        {
+
+        }
+
+        public bool RunContextMenu(IWebBrowser browserControl, IBrowser browser, IFrame frame, IContextMenuParams parameters, IMenuModel model, IRunContextMenuCallback callback)
+        {
+            return false;
+        }
+    }
+}
